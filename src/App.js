9d709de17abd452b13053e2ce28b86eaf319f243
Kevin Paxton <kevin.paxton@commontime.com>
2018-02-09 13:00:54 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>We will create an awesome game with React, Redux and SVG!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
