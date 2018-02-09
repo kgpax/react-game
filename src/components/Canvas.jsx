16504996b81034f651c622934ea76211e07d4ca6
Kevin Paxton <kevin.paxton@commontime.com>
2018-02-09 15:10:04 +0000

    Make cannon move to point at mouse pointer

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
+
 import Sky from "./Sky";
 import Ground from "./Ground";
 import CannonBase from "./CannonBase";
 import CannonPipe from "./CannonPipe";
 
-const Canvas = () => {
+const Canvas = props => {
   const viewBox = [
     window.innerWidth / -2,
     100 - window.innerHeight,
@@ -16,14 +18,20 @@ const Canvas = () => {
     <svg
       id="aliens-go-home-canvas"
       preserveAspectRatio="xMaxYMax none"
+      onMouseMove={props.trackMouse}
       viewBox={viewBox}
     >
       <Sky />
       <Ground />
-      <CannonPipe rotation={45} />
+      <CannonPipe rotation={props.angle} />
       <CannonBase />
     </svg>
   );
+};
+
+Canvas.propTypes = {
+  angle: PropTypes.number.isRequired,
+  trackMouse: PropTypes.func.isRequired
 };
 
 export default Canvas;
